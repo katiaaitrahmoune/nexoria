021f1fbe25098f0ac5a707bf190f547d0b91b662
ait katia <k.aitrahmoune@esi-sba.dz>
2026-04-17 04:44:16 +0100

    adding tiw tiw

--- src/validators/app.js
+++ src/validators/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); 
 app.use('/api/scenario', senario );
 app.use('/api/portfolio', portfeil);
 app.use('/api/auth', authRoutes);
